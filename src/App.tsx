a92983f74e756d86052ee931a16967270542fe38
Azizbek:04 <150643944+az1zbek2004@users.noreply.github.com>
2024-04-26 00:24:58 +0500

    biroz o'zgartirishlar kiritildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,129 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import addIcon from "./assets/add-square.svg"
-import Column from './components/Column';
-import { ColumnWrapProps } from './components/types';
-import { nanoid } from 'nanoid';
-
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import addIcon from "./assets/add-square.svg";
+import Column from "./components/Column";
+import { ColumnWrapProps } from "./components/types";
+import { nanoid } from "nanoid";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function App() {
-  const [columnWrap, setColumnWrap] = useState<ColumnWrapProps[]>(localStorage.getItem("columns")?JSON.parse(localStorage.getItem("columns")!):[]);
+  const [columnWrap, setColumnWrap] = useState<ColumnWrapProps[]>(
+    localStorage.getItem("columns")
+      ? JSON.parse(localStorage.getItem("columns")!)
+      : []
+  );
   const [id, setId] = useState<string>("");
   function handleClick() {
-    const wrap:ColumnWrapProps = {
-      id:nanoid(),
-      tables:[]
-    }
+    const wrap: ColumnWrapProps = {
+      id: nanoid(),
+      tables: [],
+    };
     let copied = JSON.parse(JSON.stringify(columnWrap));
     copied.push(wrap);
     setColumnWrap(copied);
     localStorage.setItem("columns", JSON.stringify(copied));
   }
 
-      
   function handleDelete() {
-        let copied = JSON.parse(JSON.stringify(columnWrap));
-        copied = copied.filter((el:ColumnWrapProps) => el.id !== id); 
-        setColumnWrap(copied);
-        localStorage.setItem("columns", JSON.stringify(copied));
+    let copied = JSON.parse(JSON.stringify(columnWrap));
+    copied = copied.filter((el: ColumnWrapProps) => el.id !== id);
+    setColumnWrap(copied);
+    localStorage.setItem("columns", JSON.stringify(copied));
+  }
+
+  useEffect(() => {
+    if (id !== "") {
+      handleDelete();
+    }
+  }, [id, columnWrap]);
+
+  const handleDragDrop = (results: any) => {
+    const { source, destination, type } = results;
+
+    if (!destination) return;
+
+    if (
+      source.draggableId === destination.draggableId &&
+      source.index === destination.index
+    )
+      return;
+
+    if (type === "group") {
+      const reorderedStores = [...columnWrap];
+
+      const sourceIndex = source.index;
+      const destinationIndex = destination.index;
+
+      const [removedStore] = reorderedStores.splice(sourceIndex, 1);
+      reorderedStores.splice(destinationIndex, 0, removedStore);
+
+      return setColumnWrap(reorderedStores);
     }
 
-    useEffect(() => {
-      handleDelete();
-    }, [id, columnWrap])
+    const storeSourceIndex = columnWrap.findIndex(
+      (el) => el.id === source.id
+    );
+    const storeDestinationIndex = columnWrap.findIndex(
+      (store) => store.id === destination.droppableId
+    );
+    const newSourceItems = [...columnWrap[storeSourceIndex].tables];
+    const newDestinitionItems =
+      source.droppableProps !== destination.droppableProps
+        ? [...columnWrap[storeDestinationIndex].tables]
+        : newSourceItems;
+
+    const [deletedItem] = newSourceItems.splice(source.index, 1);
+    newDestinitionItems.splice(destination.index, 0, deletedItem);
+
+    const newStores = [...columnWrap];
+
+    newStores[storeSourceIndex] = {
+      ...columnWrap[storeSourceIndex],
+      tables: newSourceItems,
+    };
+    newStores[storeDestinationIndex] = {
+      ...columnWrap[storeDestinationIndex],
+      tables: newDestinitionItems,
+    };
+
+    setColumnWrap(newStores)
+  };
 
   return (
     <>
-     <div className='project'>
-      <h1>Loyiha  ketma-ketligi</h1>
-      <div className='project-column'>
-        {
-          columnWrap && columnWrap.map(el => (
-            <Column setIds={setId} key={el.id} id={el.id} tables={el.tables} setColumnWrap={setColumnWrap}/>
-          ))
-        }
+      <div className="project">
+        <DragDropContext onDragEnd={handleDragDrop}>
+          <h1>Loyiha ketma-ketligi</h1>
+          <Droppable droppableId="ROOT" type="group">
+            {(provided) => (
+              <div
+                className="project-column"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {columnWrap &&
+                  columnWrap.map((el, index) => (
+                    <Column
+                      setIds={setId}
+                      key={el.id}
+                      id={el.id}
+                      tables={el.tables}
+                      index={index}
+                      setColumnWrap={setColumnWrap}
+                    />
+                  ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+          <div onClick={handleClick} className="add-column">
+            <img src={addIcon} alt="add icon" />
+            <h5>Ustun qo’shish</h5>
+          </div>
+        </DragDropContext>
       </div>
-      <div onClick={handleClick} className="add-column">
-        <img src={addIcon} alt="add icon" />
-        <h5>Ustun qo’shish</h5>
-      </div>
-     </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Column/index.tsx
+++ src/components/Column/index.tsx
@@ -1,184 +1,304 @@
 import "./index.css";
-import menu from "../../assets/delete.svg"
+import menu from "../../assets/delete.svg";
 import React, { useRef, useState } from "react";
-import {nanoid} from "nanoid";
+import { nanoid } from "nanoid";
 import { ColumnProps, ColumnWrapProps } from "../types";
+import { Draggable, Droppable } from "react-beautiful-dnd";
 
 interface typeProps {
-    id:string;
-    tables:ColumnProps[];
-    setColumnWrap: (data: ColumnWrapProps[]) => void;
-    setIds: (data: string) => void;
+  id: string;
+  tables: ColumnProps[];
+  setColumnWrap: (data: ColumnWrapProps[]) => void;
+  setIds: (data: string) => void;
+  index: number;
 }
 
-const Column: React.FC<typeProps> = ({id, tables, setColumnWrap, setIds}) => {
-    const nameRef = useRef<HTMLInputElement>(null);
-    const keyRef = useRef<HTMLInputElement>(null);
-    const name = useRef<HTMLInputElement>(null);                   
-    const key = useRef<HTMLInputElement>(null);
-    const stored = localStorage.getItem("id") ? JSON.parse( localStorage.getItem("id")!) : "" ;
-    const [ids, setId] = useState<string >(stored);
-    const columnStored = localStorage.getItem("columns")? JSON.parse(localStorage.getItem("columns")!):[]
-    const [columns, setColumns] = useState<ColumnProps[]>(columnStored);
+const Column: React.FC<typeProps> = ({
+  id,
+  tables,
+  setColumnWrap,
+  setIds,
+  index,
+}) => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const keyRef = useRef<HTMLInputElement>(null);
+  const name = useRef<HTMLInputElement>(null);
+  const key = useRef<HTMLInputElement>(null);
+  const stored = localStorage.getItem("id")
+    ? JSON.parse(localStorage.getItem("id")!)
+    : "";
+  const [ids, setId] = useState<string>(stored);
+  const columnStored = localStorage.getItem("columns")
+    ? JSON.parse(localStorage.getItem("columns")!)
+    : [];
+  const [columns, setColumns] = useState<ColumnProps[]>(columnStored);
 
-    function validate(name: HTMLInputElement | null, key: HTMLInputElement | null) {
-        if (!name?.value.trim()) {
-            alert("Name bo'sh bo'lishi mumkin emas!");
-            name?.focus();
-            return false;
+  function validate(
+    name: HTMLInputElement | null,
+    key: HTMLInputElement | null
+  ) {
+    if (!name?.value.trim()) {
+      alert("Name bo'sh bo'lishi mumkin emas!");
+      name?.focus();
+      return false;
+    }
+    if (!key?.value.trim()) {
+      alert("Key bo'sh bo'lishi mumkin emas!");
+      key?.focus();
+      return false;
+    }
+    return true;
+  }
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const isValid = validate(nameRef.current, keyRef.current);
+    if (isValid) {
+      const column: ColumnProps = {
+        id: nanoid(),
+        name: nameRef.current?.value,
+        key: keyRef.current?.value,
+      };
+      let copied = JSON.parse(JSON.stringify(columns));
+      copied = copied.map((el: ColumnWrapProps) => {
+        if (el.id === id) {
+          el.tables.push(column);
         }
-        if (!key?.value.trim()) {
-            alert("Key bo'sh bo'lishi mumkin emas!");
-            key?.focus();
-            return false;
+        return el;
+      });
+      setColumns(copied);
+      setColumnWrap(copied);
+      localStorage.setItem("columns", JSON.stringify(copied));
+      nameRef.current!.value = "";
+      keyRef.current!.value = "";
+    }
+  }
+
+  function handleDelete() {
+    const isDelete = confirm("Rostdan ham o'chirmoqchimisiz?");
+    if (isDelete) {
+      setIds(id);
+    }
+  }
+
+  function Deleted(elId: string) {
+    const isDelete = confirm("Rostdan ham o'chirmoqchimisiz?");
+    if (isDelete) {
+      let copied = JSON.parse(JSON.stringify(columns));
+      copied = copied.map((el: ColumnWrapProps) => {
+        if (el.id === id) {
+          el.tables = el.tables.filter((item) => item.id !== elId);
         }
-        return true;
+        return el;
+      });
+      setColumns(copied);
+      setColumnWrap(copied);
+      localStorage.setItem("columns", JSON.stringify(copied));
     }
+  }
 
-    function handleSubmit(e: React.FormEvent) {
-        e.preventDefault();
-        const isValid = validate(nameRef.current, keyRef.current);
-        if (isValid) {
-            const column:ColumnProps = {
-                id:nanoid(),
-                name:nameRef.current?.value,
-                key:keyRef.current?.value
-            }
-            let copied = JSON.parse(JSON.stringify(columns))
-            copied = copied.map((el:ColumnWrapProps) => {
-                if (el.id === id) {
-                    el.tables.push(column);                    
-                }
-                return el;
-            })         
-            setColumns(copied);
-            setColumnWrap(copied);
-            localStorage.setItem("columns", JSON.stringify(copied));
-            nameRef.current!.value = ''
-            keyRef.current!.value = ''
+  function Edited(el: ColumnProps) {
+    setId(el.id);
+    localStorage.setItem("id", JSON.stringify(el.id));
+  }
+
+  function handleEdit() {
+    let copied = JSON.parse(JSON.stringify(columns));
+
+    copied = copied.map((el: ColumnWrapProps) => {
+      el.tables = el.tables.map((item) => {
+        if (item.id == ids) {
+          item.name = name.current?.value ? name.current?.value : item.name;
+          item.key = key.current?.value ? key.current?.value : item.key;
         }
-    }
-    
-    function handleDelete() {
-        const isDelete = confirm("Rostdan ham o'chirmoqchimisiz?");
-        if (isDelete) {
-        setIds(id);
-    }
-    }
-    function Deleted(elId:string) {
-        const isDelete = confirm("Rostdan ham o'chirmoqchimisiz?");
-        if (isDelete) {
-            let copied = JSON.parse(JSON.stringify(columns));
-            copied = copied.map((el:ColumnWrapProps) => {
-                if (el.id === id) {
-                   el.tables = el.tables.filter(item => item.id !== elId)                    
-                }
-                return el;
-            });
-            setColumns(copied);
-            setColumnWrap(copied);
-            localStorage.setItem("columns", JSON.stringify(copied));      
-    }
-    }
-    
-    function Edited(el: ColumnProps) {
-        setId(el.id);
-        localStorage.setItem("id", JSON.stringify(el.id));
-    }
-    
-    
 
-    function handleEdit() {
-        let copied = JSON.parse(JSON.stringify(columns));
-        
-        copied = copied.map((el:ColumnWrapProps) => {
-                
-                el.tables = el.tables.map(item => {
-                                        
-                    if (item.id == ids) {
-                        item.name = name.current?.value ? name.current?.value : item.name;
-                        item.key = key.current?.value ? key.current?.value : item.key;
-                    }
+        return item;
+      });
 
-                    return item;
-                });
-                
-            return el;
-        });                    
-        setColumns(copied);
-        setColumnWrap(copied);
-        localStorage.setItem("columns", JSON.stringify(copied));
-    }
-    
-    return (
-        <div className="column-wrap">
+      return el;
+    });
+    setColumns(copied);
+    setColumnWrap(copied);
+    localStorage.setItem("columns", JSON.stringify(copied));
+  }
 
-        <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-            <div className="modal-content">
-            <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">Edit message</h1>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  return (
+    <Draggable draggableId={id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+        >
+          <div className="column-wrap">
+            <div
+              className="modal fade"
+              id="exampleModal"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">
+                      Edit message
+                    </h1>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    <form>
+                      <div className="mb-3">
+                        <label
+                          htmlFor="recipient-name"
+                          className="col-form-label"
+                        >
+                          Name:
+                        </label>
+                        <input
+                          ref={name}
+                          type="text"
+                          className="form-control"
+                          id="recipient-name"
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label
+                          htmlFor="recipient-name"
+                          className="col-form-label"
+                        >
+                          Key:
+                        </label>
+                        <input
+                          ref={key}
+                          type="password"
+                          className="form-control"
+                          id="recipient-name"
+                        />
+                      </div>
+                    </form>
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      Close
+                    </button>
+                    <button
+                      onClick={handleEdit}
+                      type="button"
+                      className="btn btn-primary"
+                      data-bs-dismiss="modal"
+                    >
+                      Save message
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="modal-body">
-                <form>
-                    <div className="mb-3">
-                        <label htmlFor="recipient-name" className="col-form-label">Name:</label>
-                        <input ref={name} type="text" className="form-control" id="recipient-name"/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="recipient-name" className="col-form-label">Key:</label>
-                        <input ref={key} type="password" className="form-control" id="recipient-name"/>
-                    </div>
-                </form>
-            </div>
-            <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button onClick={handleEdit} type="button" className="btn btn-primary" data-bs-dismiss="modal">Save message</button>
-            </div>
-            </div>
-        </div>
-        </div>
 
             <form onSubmit={handleSubmit} name="form" className="column">
-                <img onClick={handleDelete} src={menu} width={32} height={32} alt="menu icon" />
-                <div className="input-column">
-                    <label htmlFor="ustun">Ustun nomi</label>
-                    <input ref={nameRef} id="ustun" type="text" placeholder="Ustun nomi"/>
-                </div>
-                <div className="input-key">
-                    <label htmlFor="key">Key</label>
-                    <input ref={keyRef} id="key" type="password" placeholder="Key"/>
-                </div>
-                <button type="submit" className="d-none"></button>
+              <img
+                onClick={handleDelete}
+                src={menu}
+                width={32}
+                height={32}
+                alt="menu icon"
+              />
+              <div className="input-column">
+                <label htmlFor="ustun">Ustun nomi</label>
+                <input
+                  ref={nameRef}
+                  id="ustun"
+                  type="text"
+                  placeholder="Ustun nomi"
+                />
+              </div>
+              <div className="input-key">
+                <label htmlFor="key">Key</label>
+                <input
+                  ref={keyRef}
+                  id="key"
+                  type="password"
+                  placeholder="Key"
+                />
+              </div>
+              <button type="submit" className="d-none"></button>
             </form>
             <table className="table">
-                <thead className="table-light text-center">
-                    <tr>
-                        <th className="text-start px-3">N%</th>
-                        <th>Ustun nomi</th>
-                        <th>Key</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody className="table text-center">
-                    {
-                        tables && tables.map((el, index) => (
-                            <tr key={el.id}>
-                                <td className="text-start px-3">{index + 1}</td>
-                                <td>{el.name}</td>
-                                <td>{el.key}</td>
-                                <td className="d-flex justify-content-center m-0 p-0">
-                                    <span data-bs-toggle="modal" data-bs-target= "#exampleModal" data-bs-whatever="@fat" onClick={() => {Edited(el)}} className="text-danger">edit</span>
-                                    <span onClick={() => {Deleted(el.id)}} className="text-success">trash</span>
-                                </td>
+              <thead className="table-light text-center">
+                <tr>
+                  <th className="text-start px-3">N%</th>
+                  <th>Ustun nomi</th>
+                  <th>Key</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+
+              <Droppable droppableId={id}>
+                {(provided) => (
+                  <tbody
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className="table text-center"
+                  >
+                    {tables &&
+                      tables.map((el, index) => (
+                        <Draggable
+                          draggableId={el.id}
+                          key={el.id}
+                          index={index}
+                        >
+                          {(provided) => (
+                            <tr
+                              {...provided.dragHandleProps}
+                              {...provided.draggableProps}
+                              ref={provided.innerRef}
+                            >
+                              <td className="text-start px-3">{index + 1}</td>
+                              <td>{el.name}</td>
+                              <td>{el.key}</td>
+                              <td className="d-flex justify-content-center m-0 p-0">
+                                <span
+                                  data-bs-toggle="modal"
+                                  data-bs-target="#exampleModal"
+                                  data-bs-whatever="@fat"
+                                  onClick={() => {
+                                    Edited(el);
+                                  }}
+                                  className="text-danger"
+                                >
+                                  edit
+                                </span>
+                                <span
+                                  onClick={() => {
+                                    Deleted(el.id);
+                                  }}
+                                  className="text-success"
+                                >
+                                  trash
+                                </span>
+                              </td>
                             </tr>
-                        ))
-                    }
-                </tbody>
+                          )}
+                        </Draggable>
+                      ))}
+                    {provided.placeholder}
+                  </tbody>
+                )}
+              </Droppable>
             </table>
+          </div>
         </div>
-    );
-}
+      )}
+    </Draggable>
+  );
+};
 
 export default Column;
